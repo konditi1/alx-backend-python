b94456cfab59a637bf739cfbaefbedda4a188900
konditi1 <konditi1@gmail.com>
2024-01-13 12:59:37 +0300

    type notation

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -2,16 +2,16 @@
 """
 type notation for the function
 """
-from typing import TypeVar, Mapping, Union
+from typing import TypeVar, Mapping, Any, Union
 
 K = TypeVar('K')
-V = TypeVar('V')
+V = TypeVar('V', covariant=True)
 
 
 def safely_get_value(dct: Mapping[K, V], key: K, default: Union[V, None] = None) -> Union[V, None]:
     """
     Return the value associated with the key in the mapping,
-    or the default value if the key is not present
+    or the default value if the key is not present.
     """
     if key in dct:
         return dct[key]
