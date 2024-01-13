84f771b898228f57153b32783d4c013833bc186d
konditi1 <konditi1@gmail.com>
2024-01-13 10:31:12 +0300

    returns a floor of a float

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -3,7 +3,7 @@
 a type-annotated function floor that takes a float n as argument
 and returns the floor of the float.
 """
-from math import floor
+from math import floor as math_floor
 
 
 def floor(n: float) -> int:
@@ -11,4 +11,4 @@ def floor(n: float) -> int:
     a type-annotated function floor that takes a float n as argument
     and returns the floor of the float.
     """
-    return floor(n)
+    return math_floor(n)
