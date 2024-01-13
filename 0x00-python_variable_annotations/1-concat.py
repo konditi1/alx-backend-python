08a74bd79c05647d8dd7a94b38418f8dd244d22f
konditi1 <konditi1@gmail.com>
2024-01-13 10:17:48 +0300

    cocatinates two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+a type-annotated function concat that takes a string str1 and a
+string str2 as arguments and returns their concatenation
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    a type-annotated function concat that takes a string str1 and a
+    string str2 as arguments and returns their concatenation
+    """
+    return str1 + str2
