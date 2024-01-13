d689d9198343b8c92e47b15319a5da36297fad96
konditi1 <konditi1@gmail.com>
2024-01-13 11:56:16 +0300

    type notation

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -2,13 +2,13 @@
 """
 type notation for the following function
 """
-from typing import TypeVar, Dict, Optional
+from typing import TypeVar, Dict, Optional, Union
 
 K = TypeVar('K')
 V = TypeVar('V')
 
 
-def safely_get_value(dct: Dict[K, V], key: K, default: Optional[V] = None) -> V:
+def safely_get_value(dct: Dict[K, V], key: K, default: Optional[V] = None) -> Union[V, None]:
     """
     Return the value associated with the key in the dictionary,
     or the default value if the key is not present.
