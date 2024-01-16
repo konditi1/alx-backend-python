0b27436fc6ab1a972a6d1bd9721284dc4da73d9c
konditi1 <konditi1@gmail.com>
2024-01-16 22:46:56 +0300

    Coroutine that executes async_comprehension four times in parallel     using asyncio.gather

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+import 1-async_comprehension.py
+"""
+import asyncio
+import time
+from typing import List
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Coroutine that executes async_comprehension four times in parallel
+    using asyncio.gather
+    """
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end_time = time.time()
+    return end_time - start_time
