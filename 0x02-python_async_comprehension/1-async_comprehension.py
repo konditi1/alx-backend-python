09f0ece98b5853d97d586c91c815f0672de0ce24
konditi1 <konditi1@gmail.com>
2024-01-16 22:43:49 +0300

    imports from taskzero then returns a list comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,10 +2,12 @@
 """
 Import async_generator
 """
+from typing import List
+
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list:
+async def async_comprehension() -> List[float]:
     """
     coroutine that takes no arguments
     """
