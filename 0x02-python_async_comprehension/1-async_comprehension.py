bc02d0c7ab6b996ea4d6388858767e6c58150aa2
konditi1 <konditi1@gmail.com>
2024-01-16 22:34:23 +0300

    added a comment

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -6,5 +6,8 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension():
+    """
+    coroutine that takes no arguments
+    """
     result = [i async for i in async_generator()]
     return result
