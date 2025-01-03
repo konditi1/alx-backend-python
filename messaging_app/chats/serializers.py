62ebe925f7a3c2b3649e303df9ab719cf661c7ae
konditi1 <konditi1@gmail.com>
2025-01-03 20:00:55 +0300

    correct typo

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -53,7 +53,7 @@ class MessageSerializer(serializers.ModelSerializer):
 
 class ConversationSerializer(serializers.ModelSerializer):
     participants = UserSerializer(many=True, read_only=True)
-    message = MessageSerializer(many=True, read_only=True)
+    messages = MessageSerializer(many=True, read_only=True)
 
     class Meta:
         model = Conversation
